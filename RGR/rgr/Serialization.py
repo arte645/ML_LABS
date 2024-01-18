823047f998ffd803700b936b8137f66af48aab23
Artyom <ovs.artm@gmail.com>
2024-01-18 22:01:19 +0600

    first commit

---
+++ RGR/rgr/Serialization.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("Разработка Web-приложения (дашборда) для инференса (вывода) моделей ML и анализа данных")
+
+
+st.header("Автор")
+st.write("ФИО: Раевских Максим Игоревич")
+st.write("Группа: ФИТ-222")
+st.write("2023 год")
+!!streamlit run C:\Users\1\Jupyter Notebook\venv\notebooks\RGR\rgr\Serialization.py
